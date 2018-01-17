d7cbe513ec9986dce5c6ebac168b4bd468080e58
Tomasz Koscielniak <tomek.maq@gmail.com>
2018-01-17 20:25:56 +0100

    difference-of-squares

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,22 @@
 def square_of_sum(count):
-    pass
+    sum = 0
+    while count > 0:
+        sum += count
+        count -= 1
+    return sum ** 2
 
 
 def sum_of_squares(count):
-    pass
+    sum = 0
+    while count > 0:
+        sum += count ** 2
+        count -= 1
+    return sum
 
 
 def difference(count):
-    pass
+    return square_of_sum(count) - sum_of_squares(count)
+
+
+print(square_of_sum(10))
+print(sum_of_squares(10))
