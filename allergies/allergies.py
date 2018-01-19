ff98e15617e8d17c29f7d177bf6429a09356f77b
Tomasz Koscielniak <tomek.maq@gmail.com>
2018-01-19 21:23:36 +0100

    g 	allergies

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,11 +1,24 @@
 class Allergies(object):
+    allergens = {'eggs': 1,
+                 'peanuts': 2,
+                 'shellfish': 4,
+                 'strawberries': 8,
+                 'tomatoes': 16,
+                 'chocolate': 32,
+                 'pollen': 64,
+                 'cats': 128}
 
     def __init__(self, score):
-        pass
+        self.score = score
 
     def is_allergic_to(self, item):
-        pass
+        return Allergies.allergens.get(item) & self.score > 0
 
     @property
     def lst(self):
-        pass
+        return [key for key in Allergies.allergens.keys() if Allergies.allergens.get(key) & self.score > 0]
+
+
+a = Allergies(254)
+print(a.lst)
+
