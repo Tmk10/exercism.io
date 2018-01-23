2a7170083ce6e8287bb552a16f9cacaf193c8d70
Tomasz Koscielniak <tomek.maq@gmail.com>
2018-01-23 19:40:59 +0100

    acronym

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,5 +1,11 @@
 import re
+from string import punctuation
 
 def abbreviate(words):
-    
-    pass
+    words = re.findall("\w+",words)
+    print(words)
+    result_string = ""
+    for word in words:
+        result_string += word[0].upper()
+    return result_string
+
