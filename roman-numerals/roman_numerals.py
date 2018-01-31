b75c9b99eb1f106781c07401dd72e4988357ecde
Tomasz Koscielniak <tomek.maq@gmail.com>
2018-01-31 17:59:24 +0100

    roman numbers converter

---
+++ roman-numerals/roman_numerals.py
@@ -0,0 +1,12 @@
+def numeral(number):
+    result = ""
+    roman_dict = {1000: 'M', 900: "CM", 500: 'D', 400: "CD", 100: 'C', 90: "XC", 50: 'L', 40: "XL", 10: 'X', 9: "IX",
+                  5: 'V', 4: "IV", 1: 'I'}
+    for key in roman_dict.keys():
+        quotient = divmod(number, key)[0]
+        if quotient > 0:
+            result += quotient * roman_dict[key]
+            number -= quotient * key
+    return result
+
+print(numeral(2280))
