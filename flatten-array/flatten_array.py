6ad6b6fb41d1b684e049abaa90bc679d569218aa
Tomasz Koscielniak <tomek.maq@gmail.com>
2018-01-31 17:23:38 +0100

    flatten-array

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -1,17 +1,11 @@
-def flatten(iterable, flatten_array =[]):
-    while iterable:
-        if not isinstance(iterable[0], list):
-            if iterable[0] !=None:
-                flatten_array.append(iterable[0])
-            iterable = iterable[1:]
+def flatten(iterable):
+    if iterable is None:
+        return []
+    flatten_list = []
+    for item in iterable:
+        flatten_list.append(item) if type(item) in [int, str] else flatten_list.extend(flatten(item))
+    return flatten_list
 
-        else:
-            flatten(iterable[0])
-            iterable = iterable[1:]
-
-    return flatten_array
-
-print(flatten([[0, 2, [[2, 3], 8, [[100]], None, [[None]]], -2]]))
 
 
 
