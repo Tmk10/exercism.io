acdc4d783585ab58ebe10f3df8741470e730e074
Tomasz Koscielniak <tomek.maq@gmail.com>
2018-02-02 22:56:35 +0100

    prime factors

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -1,13 +1,10 @@
 def prime_factors(natural_number):
     result =[]
-    divider = 2
-    if natural_number == 1:
-        return []
-    else:
-        while natural_number > 1:
-            if divmod(natural_number, divider)[1] == 0:
-                result.append(divider)
-                natural_number = natural_number // divider
-            else:
-                divider +=1
-        return result
+    divider =2
+    while natural_number > 1:
+        if divmod(natural_number, divider)[1] == 0:
+            result.append(divider)
+            natural_number = natural_number // divider
+        else:
+            divider +=1
+    return result
