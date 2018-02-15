f945d142fd7c941a9ed87d0b0793921a79888f35
Tomasz Koscielniak <tomek.maq@gmail.com>
2018-02-15 21:15:26 +0100

    matrix v2

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,11 +1,15 @@
 class Matrix(object):
 
+
     def __init__(self, matrix_string):
-        self.matrix = matrix_string.split("\n")
-        self.matrix = [item.split(" ") for item in self.matrix]
-        self.matrix = [[int(x) for x in element if x.isdigit()] for element in self.matrix]
+        self.matrix = [[int(x) for x in element.split(" ") if x.isdigit()] for element in matrix_string.splitlines()]
         self.rows = self.matrix
         self.columns = [list(x) for x in zip(*self.matrix)]
 
 
 
+"""self.matrix = matrix_string.split("\n")
+        self.matrix = [item.split(" ") for item in self.matrix]"""
+
+a= Matrix("1 2 3\n4 5 6\n7 8 9\n8 7 6")
+print(a.matrix)
