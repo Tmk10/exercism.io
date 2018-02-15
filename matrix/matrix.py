630680e98abb36d4bf0bd794d7da8882bc5df1bd
Tomasz Koscielniak <tomek.maq@gmail.com>
2018-02-15 21:20:53 +0100

    matrix.v2

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -6,10 +6,3 @@ class Matrix(object):
         self.rows = self.matrix
         self.columns = [list(x) for x in zip(*self.matrix)]
 
-
-
-"""self.matrix = matrix_string.split("\n")
-        self.matrix = [item.split(" ") for item in self.matrix]"""
-
-a= Matrix("1 2 3\n4 5 6\n7 8 9\n8 7 6")
-print(a.matrix)
