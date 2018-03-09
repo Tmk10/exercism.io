6f3175cd43e4cfce68b9534627ed084abcb4627b
Tomasz Koscielniak <tomek.maq@gmail.com>
2018-03-09 20:00:21 +0100

    Triangle with decorator

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,10 +1,13 @@
 from itertools import combinations
 
 
-def is_triangle(sides):
-    if not all(item >= max(sides) for item in map(sum, combinations(sides, 2))):
-        return False
-    return True
+
+def is_triangle(func):
+    def wrapper(arg):
+        if not all(item >= max(arg) for item in map(sum, combinations(arg, 2))):
+            return False
+        return func(arg)
+    return wrapper
 
 
 def is_equilateral(sides):
@@ -12,16 +15,14 @@ def is_equilateral(sides):
         return True
     return False
 
-
+@is_triangle
 def is_isosceles(sides):
-    if is_triangle(sides):
-        if len(set(sides)) == 2 or len(set(sides)) == 1:
-            return True
+    if len(set(sides)) == 2 or len(set(sides)) == 1:
+        return True
     return False
 
-
+@is_triangle
 def is_scalene(sides):
-    if is_triangle(sides):
-        if len(set(sides)) == 3:
-            return True
+    if len(set(sides)) == 3:
+        return True
     return False
